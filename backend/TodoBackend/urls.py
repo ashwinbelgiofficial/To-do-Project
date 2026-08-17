d4a593220acafb30c90cfc7b26961f2fb794dec0
Ashwin C B <ashwinbelgi.official@gmail.com>
2026-08-18 00:50:40 +0530

    Add API homepage

--- backend/TodoBackend/urls.py
+++ backend/TodoBackend/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework.authtoken.views import obtain_auth_token
 
 
+def home(request):
+    return JsonResponse({
+        "message": "Todo API is running!",
+        "api": "/api/",
+        "tasks": "/api/tasks/"
+    })
+
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-
     path('api/', include('tasks.urls')),
-
     path('api-token-auth/', obtain_auth_token),
 ]
--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r backend/requirements.txt
 python backend/manage.py collectstatic --no-input
 
 python backend/manage.py migrate
+
